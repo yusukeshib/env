18347e7678bedd17c8d94c9b33d69f784fd8a47b
Yusuke Shibata <yusukeshib@gmail.com>
2024-03-17 10:13:49 -0400

    Optimize telescope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -162,7 +162,13 @@ require('lazy').setup({
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       require("telescope").setup({
+        defaults = {
+          preview = false
+        },
         pickers = {
+          buffers = {
+            theme = "ivy",
+          },
           find_files = {
             find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
           },

ac45ddc81accb25c1a70292c2c8531f06b2dbfa3
Yusuke Shibata <yusukeshib@gmail.com>
2024-03-30 08:39:00 -0400

    Disable unneeded cmp options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -56,7 +56,6 @@ require('lazy').setup({
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
   { 'simrat39/symbols-outline.nvim', config = true },
-  { 'onsails/lspkind.nvim' },
 
   {
     "pmizio/typescript-tools.nvim",
@@ -71,8 +70,8 @@ require('lazy').setup({
   { 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-cmdline' },
   { 'hrsh7th/nvim-cmp' },
-  { 'L3MON4D3/LuaSnip' },
-  { 'saadparwaiz1/cmp_luasnip' },
+  -- { 'L3MON4D3/LuaSnip' },
+  -- { 'saadparwaiz1/cmp_luasnip' },
   { 'uga-rosa/cmp-dictionary',  config = true },
   { "zbirenbaum/copilot.lua",   config = true },
   {
@@ -224,7 +223,7 @@ require('lazy').setup({
 
 
 -- Set up nvim-cmp.
-local luasnip = require("luasnip")
+-- local luasnip = require("luasnip")
 local cmp = require('cmp')
 
 local has_words_before = function()
@@ -234,11 +233,11 @@ local has_words_before = function()
 end
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      require('luasnip').lsp_expand(args.body)
-    end,
-  },
+  -- snippet = {
+  --   expand = function(args)
+  --     require('luasnip').lsp_expand(args.body)
+  --   end,
+  -- },
   window = {
     -- completion = cmp.config.window.bordered(),
     -- documentation = cmp.config.window.bordered(),
@@ -252,10 +251,8 @@ cmp.setup({
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-        -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-        -- that way you will only jump inside the snippet region
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
+        -- elseif luasnip.expand_or_jumpable() then
+        --   luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
       else
@@ -265,8 +262,8 @@ cmp.setup({
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
+        -- elseif luasnip.jumpable(-1) then
+        --   luasnip.jump(-1)
       else
         fallback()
       end
@@ -276,7 +273,7 @@ cmp.setup({
     { name = "copilot" },
     { name = "git" },
     { name = 'nvim_lsp' },
-    { name = 'luasnip' },
+    -- { name = 'luasnip' },
     {
       name = "dictionary",
       keyword_length = 2,
@@ -286,36 +283,23 @@ cmp.setup({
   }, {
     { name = 'buffer' },
   }),
-  formatting = {
-    format = function(entry, vim_item)
-      if vim.tbl_contains({ 'path' }, entry.source.name) then
-        local icon, hl_group = require('nvim-web-devicons').get_icon(entry:get_completion_item().label)
-        if icon then
-          vim_item.kind = icon
-          vim_item.kind_hl_group = hl_group
-          return vim_item
-        end
-      end
-      return require('lspkind').cmp_format({ with_text = false, maxwidth = 50, })(entry, vim_item)
-    end
-  }
 })
 
 -- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'git' },
-    { name = 'gitmoji' },
-    {
-      name = "dictionary",
-      keyword_length = 2,
-    },
-    { name = "rg" },
-    { name = "copilot" },
-  }, {
-    { name = 'buffer' },
-  })
-})
+-- cmp.setup.filetype('gitcommit', {
+--   sources = cmp.config.sources({
+--     { name = 'git' },
+--     { name = 'gitmoji' },
+--     {
+--       name = "dictionary",
+--       keyword_length = 2,
+--     },
+--     { name = "rg" },
+--     { name = "copilot" },
+--   }, {
+--     { name = 'buffer' },
+--   })
+-- })
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
 cmp.setup.cmdline({ '/', '?' }, {
   mapping = cmp.mapping.preset.cmdline(),
@@ -325,14 +309,14 @@ cmp.setup.cmdline({ '/', '?' }, {
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
+-- cmp.setup.cmdline(':', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = cmp.config.sources({
+--     { name = 'path' }
+--   }, {
+--     { name = 'cmdline' }
+--   })
+-- })
 
 require("cmp_git").setup()
 

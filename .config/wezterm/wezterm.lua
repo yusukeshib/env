84160e1233b12e77bcda45d0257f4ce12c6a9f43
Yusuke Shibata <yusukeshib@gmail.com>
2024-07-05 12:45:33 -0400

    Wezterm config

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,14 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+-- config.color_scheme = 'Deep'
+config.font = wezterm.font('Hack Nerd Font Mono')
+config.font_size = 14.0
+config.initial_rows = 68
+config.initial_cols = 210
+config.enable_tab_bar = false
+config.adjust_window_size_when_changing_font_size = false
+config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }
+
+return config

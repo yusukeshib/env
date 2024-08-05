62c721ea7a2cecd98d4b6686bcfbc976205edd13
Yusuke Shibata <yusukeshib@gmail.com>
2024-08-05 08:21:33 -0400

    Adjust style for font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,9 +4,9 @@ local config = wezterm.config_builder()
 
 -- config.color_scheme = 'Dracula'
 config.font = wezterm.font('MonoLisa Nerd Font Mono')
-config.font_size = 15.0
-config.initial_rows = 80
-config.initial_cols = 200
+config.font_size = 12.7
+config.initial_rows = 84
+config.initial_cols = 216
 config.enable_tab_bar = false
 config.adjust_window_size_when_changing_font_size = false
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0, }

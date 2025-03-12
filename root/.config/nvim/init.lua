5b2f363771542a75d9afd45b000f10c018be665c
Yusuke Shibata <yusukeshib@gmail.com>
2025-03-11 20:07:34 -0400

    copilot

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -753,7 +753,10 @@ require('lazy').setup({
   {
     'zbirenbaum/copilot.lua',
     config = function()
-      require('copilot').setup()
+      require('copilot').setup {
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      }
     end,
   },
 
@@ -813,7 +816,24 @@ require('lazy').setup({
           end,
         },
         completion = { completeopt = 'menu,menuone,noinsert' },
+        sorting = {
+          priority_weight = 2,
+          comparators = {
+            require('copilot_cmp.comparators').prioritize,
 
+            -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
+        },
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
@@ -893,7 +913,7 @@ require('lazy').setup({
           end, { 'i', 's' }),
         },
         sources = {
-          -- { name = 'copilot' },
+          { name = 'copilot' },
           {
             name = 'lazydev',
             -- set group index to 0 to skip loading LuaLS completions as lazydev recommends it
@@ -1082,189 +1102,189 @@ require('lazy').setup({
       },
     },
   },
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    dependencies = {
-      { 'github/copilot.vim' },
-      { 'nvim-lua/plenary.nvim' },
-    },
-    opts = {
-      question_header = '## User ',
-      answer_header = '## Copilot ',
-      error_header = '## Error ',
-      prompts = {
-        -- Code related prompts
-        Explain = 'Please explain how the following code works.',
-        Review = 'Please review the following code and provide suggestions for improvement.',
-        Tests = 'Please explain how the selected code works, then generate unit tests for it.',
-        Refactor = 'Please refactor the following code to improve its clarity and readability.',
-        FixCode = 'Please fix the following code to make it work as intended.',
-        FixError = 'Please explain the error in the following text and provide a solution.',
-        BetterNamings = 'Please provide better names for the following variables and functions.',
-        Documentation = 'Please provide documentation for the following code.',
-        SwaggerApiDocs = 'Please provide documentation for the following API using Swagger.',
-        SwaggerJsDocs = 'Please write JSDoc for the following API using Swagger.',
-        -- Text related prompts
-        Summarize = 'Please summarize the following text.',
-        Spelling = 'Please correct any grammar and spelling errors in the following text.',
-        Wording = 'Please improve the grammar and wording of the following text.',
-        Concise = 'Please rewrite the following text to make it more concise.',
-      },
-      model = 'claude-3.7-sonnet',
-      auto_follow_cursor = false, -- Don't follow the cursor after getting response
-      mappings = {
-        -- Use tab for completion
-        complete = {
-          detail = 'Use @<Tab> or /<Tab> for options.',
-          insert = '<Tab>',
-        },
-        -- Close the chat
-        close = {
-          normal = 'q',
-          insert = '<C-c>',
-        },
-        -- Reset the chat buffer
-        reset = {
-          normal = '<C-x>',
-          insert = '<C-x>',
-        },
-        -- Submit the prompt to Copilot
-        submit_prompt = {
-          normal = '<CR>',
-          insert = '<C-CR>',
-        },
-        -- Accept the diff
-        accept_diff = {
-          normal = '<C-y>',
-          insert = '<C-y>',
-        },
-        -- Show help
-        show_help = {
-          normal = 'g?',
-        },
-      },
-    },
-    config = function(_, opts)
-      local chat = require 'CopilotChat'
-      chat.setup(opts)
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   dependencies = {
+  --     { 'github/copilot.vim' },
+  --     { 'nvim-lua/plenary.nvim' },
+  --   },
+  --   opts = {
+  --     question_header = '## User ',
+  --     answer_header = '## Copilot ',
+  --     error_header = '## Error ',
+  --     prompts = {
+  --       -- Code related prompts
+  --       Explain = 'Please explain how the following code works.',
+  --       Review = 'Please review the following code and provide suggestions for improvement.',
+  --       Tests = 'Please explain how the selected code works, then generate unit tests for it.',
+  --       Refactor = 'Please refactor the following code to improve its clarity and readability.',
+  --       FixCode = 'Please fix the following code to make it work as intended.',
+  --       FixError = 'Please explain the error in the following text and provide a solution.',
+  --       BetterNamings = 'Please provide better names for the following variables and functions.',
+  --       Documentation = 'Please provide documentation for the following code.',
+  --       SwaggerApiDocs = 'Please provide documentation for the following API using Swagger.',
+  --       SwaggerJsDocs = 'Please write JSDoc for the following API using Swagger.',
+  --       -- Text related prompts
+  --       Summarize = 'Please summarize the following text.',
+  --       Spelling = 'Please correct any grammar and spelling errors in the following text.',
+  --       Wording = 'Please improve the grammar and wording of the following text.',
+  --       Concise = 'Please rewrite the following text to make it more concise.',
+  --     },
+  --     model = 'claude-3.7-sonnet',
+  --     auto_follow_cursor = false, -- Don't follow the cursor after getting response
+  --     mappings = {
+  --       -- Use tab for completion
+  --       complete = {
+  --         detail = 'Use @<Tab> or /<Tab> for options.',
+  --         insert = '<Tab>',
+  --       },
+  --       -- Close the chat
+  --       close = {
+  --         normal = 'q',
+  --         insert = '<C-c>',
+  --       },
+  --       -- Reset the chat buffer
+  --       reset = {
+  --         normal = '<C-x>',
+  --         insert = '<C-x>',
+  --       },
+  --       -- Submit the prompt to Copilot
+  --       submit_prompt = {
+  --         normal = '<CR>',
+  --         insert = '<C-CR>',
+  --       },
+  --       -- Accept the diff
+  --       accept_diff = {
+  --         normal = '<C-y>',
+  --         insert = '<C-y>',
+  --       },
+  --       -- Show help
+  --       show_help = {
+  --         normal = 'g?',
+  --       },
+  --     },
+  --   },
+  --   config = function(_, opts)
+  --     local chat = require 'CopilotChat'
+  --     chat.setup(opts)
 
-      local select = require 'CopilotChat.select'
-      vim.api.nvim_create_user_command('CopilotChatVisual', function(args)
-        chat.ask(args.args, { selection = select.visual })
-      end, { nargs = '*', range = true })
+  --     local select = require 'CopilotChat.select'
+  --     vim.api.nvim_create_user_command('CopilotChatVisual', function(args)
+  --       chat.ask(args.args, { selection = select.visual })
+  --     end, { nargs = '*', range = true })
 
-      -- Inline chat with Copilot
-      vim.api.nvim_create_user_command('CopilotChatInline', function(args)
-        chat.ask(args.args, {
-          selection = select.visual,
-          window = {
-            layout = 'float',
-            relative = 'cursor',
-            width = 1,
-            height = 0.4,
-            row = 1,
-          },
-        })
-      end, { nargs = '*', range = true })
+  --     -- Inline chat with Copilot
+  --     vim.api.nvim_create_user_command('CopilotChatInline', function(args)
+  --       chat.ask(args.args, {
+  --         selection = select.visual,
+  --         window = {
+  --           layout = 'float',
+  --           relative = 'cursor',
+  --           width = 1,
+  --           height = 0.4,
+  --           row = 1,
+  --         },
+  --       })
+  --     end, { nargs = '*', range = true })
 
-      -- Restore CopilotChatBuffer
-      vim.api.nvim_create_user_command('CopilotChatBuffer', function(args)
-        chat.ask(args.args, { selection = select.buffer })
-      end, { nargs = '*', range = true })
+  --     -- Restore CopilotChatBuffer
+  --     vim.api.nvim_create_user_command('CopilotChatBuffer', function(args)
+  --       chat.ask(args.args, { selection = select.buffer })
+  --     end, { nargs = '*', range = true })
 
-      -- Custom buffer for CopilotChat
-      vim.api.nvim_create_autocmd('BufEnter', {
-        pattern = 'copilot-*',
-        callback = function()
-          vim.opt_local.relativenumber = true
-          vim.opt_local.number = true
+  --     -- Custom buffer for CopilotChat
+  --     vim.api.nvim_create_autocmd('BufEnter', {
+  --       pattern = 'copilot-*',
+  --       callback = function()
+  --         vim.opt_local.relativenumber = true
+  --         vim.opt_local.number = true
 
-          -- Get current filetype and set it to markdown if the current filetype is copilot-chat
-          local ft = vim.bo.filetype
-          if ft == 'copilot-chat' then
-            vim.bo.filetype = 'markdown'
-          end
-        end,
-      })
-    end,
-    event = 'VeryLazy',
-    keys = {
-      -- Show prompts actions with telescope
-      {
-        '<leader>ccp',
-        function()
-          local actions = require 'CopilotChat.actions'
-          require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
-        end,
-        desc = 'CopilotChat - Prompt actions',
-      },
-      {
-        '<leader>ccp',
-        ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
-        mode = 'x',
-        desc = 'CopilotChat - Prompt actions',
-      },
-      -- Code related commands
-      { '<leader>cce', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
-      { '<leader>cct', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests' },
-      { '<leader>ccr', '<cmd>CopilotChatReview<cr>', desc = 'CopilotChat - Review code' },
-      { '<leader>ccR', '<cmd>CopilotChatRefactor<cr>', desc = 'CopilotChat - Refactor code' },
-      { '<leader>ccn', '<cmd>CopilotChatBetterNamings<cr>', desc = 'CopilotChat - Better Naming' },
-      -- Chat with Copilot in visual mode
-      {
-        '<leader>ccv',
-        ':CopilotChatVisual',
-        mode = 'x',
-        desc = 'CopilotChat - Open in vertical split',
-      },
-      {
-        '<leader>ccx',
-        ':CopilotChatInline<cr>',
-        mode = 'x',
-        desc = 'CopilotChat - Inline chat',
-      },
-      -- Custom input for CopilotChat
-      {
-        '<leader>cci',
-        function()
-          local input = vim.fn.input 'Ask Copilot: '
-          if input ~= '' then
-            vim.cmd('CopilotChat ' .. input)
-          end
-        end,
-        desc = 'CopilotChat - Ask input',
-      },
-      -- Generate commit message based on the git diff
-      {
-        '<leader>ccm',
-        '<cmd>CopilotChatCommit<cr>',
-        desc = 'CopilotChat - Generate commit message for all changes',
-      },
-      -- Quick chat with Copilot
-      {
-        '<leader>ccq',
-        function()
-          local input = vim.fn.input 'Quick Chat: '
-          if input ~= '' then
-            vim.cmd('CopilotChatBuffer ' .. input)
-          end
-        end,
-        desc = 'CopilotChat - Quick chat',
-      },
-      -- Debug
-      { '<leader>ccd', '<cmd>CopilotChatDebugInfo<cr>', desc = 'CopilotChat - Debug Info' },
-      -- Fix the issue with diagnostic
-      { '<leader>ccf', '<cmd>CopilotChatFixDiagnostic<cr>', desc = 'CopilotChat - Fix Diagnostic' },
-      -- Clear buffer and chat history
-      { '<leader>ccl', '<cmd>CopilotChatReset<cr>', desc = 'CopilotChat - Clear buffer and chat history' },
-      -- Toggle Copilot Chat Vsplit
-      { '<leader>ccv', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Toggle' },
-      -- Copilot Chat Models
-      { '<leader>cc?', '<cmd>CopilotChatModels<cr>', desc = 'CopilotChat - Select Models' },
-      -- Copilot Chat Agents
-      { '<leader>cca', '<cmd>CopilotChatAgents<cr>', desc = 'CopilotChat - Select Agents' },
-    },
-  },
+  --         -- Get current filetype and set it to markdown if the current filetype is copilot-chat
+  --         local ft = vim.bo.filetype
+  --         if ft == 'copilot-chat' then
+  --           vim.bo.filetype = 'markdown'
+  --         end
+  --       end,
+  --     })
+  --   end,
+  --   event = 'VeryLazy',
+  --   keys = {
+  --     -- Show prompts actions with telescope
+  --     {
+  --       '<leader>ccp',
+  --       function()
+  --         local actions = require 'CopilotChat.actions'
+  --         require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
+  --       end,
+  --       desc = 'CopilotChat - Prompt actions',
+  --     },
+  --     {
+  --       '<leader>ccp',
+  --       ":lua require('CopilotChat.integrations.telescope').pick(require('CopilotChat.actions').prompt_actions({selection = require('CopilotChat.select').visual}))<CR>",
+  --       mode = 'x',
+  --       desc = 'CopilotChat - Prompt actions',
+  --     },
+  --     -- Code related commands
+  --     { '<leader>cce', '<cmd>CopilotChatExplain<cr>', desc = 'CopilotChat - Explain code' },
+  --     { '<leader>cct', '<cmd>CopilotChatTests<cr>', desc = 'CopilotChat - Generate tests' },
+  --     { '<leader>ccr', '<cmd>CopilotChatReview<cr>', desc = 'CopilotChat - Review code' },
+  --     { '<leader>ccR', '<cmd>CopilotChatRefactor<cr>', desc = 'CopilotChat - Refactor code' },
+  --     { '<leader>ccn', '<cmd>CopilotChatBetterNamings<cr>', desc = 'CopilotChat - Better Naming' },
+  --     -- Chat with Copilot in visual mode
+  --     {
+  --       '<leader>ccv',
+  --       ':CopilotChatVisual',
+  --       mode = 'x',
+  --       desc = 'CopilotChat - Open in vertical split',
+  --     },
+  --     {
+  --       '<leader>ccx',
+  --       ':CopilotChatInline<cr>',
+  --       mode = 'x',
+  --       desc = 'CopilotChat - Inline chat',
+  --     },
+  --     -- Custom input for CopilotChat
+  --     {
+  --       '<leader>cci',
+  --       function()
+  --         local input = vim.fn.input 'Ask Copilot: '
+  --         if input ~= '' then
+  --           vim.cmd('CopilotChat ' .. input)
+  --         end
+  --       end,
+  --       desc = 'CopilotChat - Ask input',
+  --     },
+  --     -- Generate commit message based on the git diff
+  --     {
+  --       '<leader>ccm',
+  --       '<cmd>CopilotChatCommit<cr>',
+  --       desc = 'CopilotChat - Generate commit message for all changes',
+  --     },
+  --     -- Quick chat with Copilot
+  --     {
+  --       '<leader>ccq',
+  --       function()
+  --         local input = vim.fn.input 'Quick Chat: '
+  --         if input ~= '' then
+  --           vim.cmd('CopilotChatBuffer ' .. input)
+  --         end
+  --       end,
+  --       desc = 'CopilotChat - Quick chat',
+  --     },
+  --     -- Debug
+  --     { '<leader>ccd', '<cmd>CopilotChatDebugInfo<cr>', desc = 'CopilotChat - Debug Info' },
+  --     -- Fix the issue with diagnostic
+  --     { '<leader>ccf', '<cmd>CopilotChatFixDiagnostic<cr>', desc = 'CopilotChat - Fix Diagnostic' },
+  --     -- Clear buffer and chat history
+  --     { '<leader>ccl', '<cmd>CopilotChatReset<cr>', desc = 'CopilotChat - Clear buffer and chat history' },
+  --     -- Toggle Copilot Chat Vsplit
+  --     { '<leader>ccv', '<cmd>CopilotChatToggle<cr>', desc = 'CopilotChat - Toggle' },
+  --     -- Copilot Chat Models
+  --     { '<leader>cc?', '<cmd>CopilotChatModels<cr>', desc = 'CopilotChat - Select Models' },
+  --     -- Copilot Chat Agents
+  --     { '<leader>cca', '<cmd>CopilotChatAgents<cr>', desc = 'CopilotChat - Select Agents' },
+  --   },
+  -- },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and

8b8335bd7a5389608ea703e9daca39bc8a8d920c
Yusuke Shibata <yusukeshib@gmail.com>
2025-03-31 21:47:36 -0400

    Remove render-markdown because its annoying

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -1032,14 +1032,6 @@ require('lazy').setup({
           },
         },
       },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
     },
   },
   -- {

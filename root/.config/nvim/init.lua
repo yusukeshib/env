413eff2fa589a903c339cc738ff9c94a57253a60
Yusuke Shibata <yusukeshib@gmail.com>
2025-03-31 21:33:29 -0400

    Configure Telescope.find_files

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -342,19 +342,6 @@ require('lazy').setup({
     branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { -- If encountering errors, see telescope-fzf-native README for installation instructions
-        'nvim-telescope/telescope-fzf-native.nvim',
-
-        -- `build` is used to run some command when the plugin is installed/updated.
-        -- This is only run then, not every time Neovim starts up.
-        build = 'make',
-
-        -- `cond` is a condition used to determine whether this plugin should be
-        -- installed and loaded.
-        cond = function()
-          return vim.fn.executable 'make' == 1
-        end,
-      },
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
@@ -402,6 +389,7 @@ require('lazy').setup({
           },
           find_files = {
             theme = 'ivy',
+            hidden = true,
             layout_config = {
               height = 15,
             },
@@ -421,7 +409,6 @@ require('lazy').setup({
       }
 
       -- Enable Telescope extensions if they are installed
-      pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
 
       -- See `:help telescope.builtin`

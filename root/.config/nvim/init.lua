cc955090862b67509beb44b8197c761e41fc8a89
Yusuke Shibata <yusukeshib@gmail.com>
2025-04-03 07:46:15 -0400

    live_grep on neovim

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -387,6 +387,11 @@ require('lazy').setup({
               height = 10,
             },
           },
+          live_grep = {
+            theme = 'ivy',
+            preview = true,
+            hidden = true,
+          },
           find_files = {
             theme = 'ivy',
             hidden = true,
@@ -418,7 +423,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       -- vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>rg', builtin.live_grep, { desc = '[R]ip[G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })

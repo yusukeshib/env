3c5686d7d7df8e639c0b97f6b53c8909812d5d6d
Yusuke Shibata <yusukeshib@gmail.com>
2025-06-13 08:13:59 -0400

    gcauto

--- init.sh
+++ init.sh
@@ -13,11 +13,17 @@ if [ ! -d "$HOME/.config" ]; then
   mkdir -p $HOME/.config
   echo '-> Created .config dir.' 
 fi
+
 # Symlink all entries in the root
 cd $HOME/env/root
 find . -maxdepth 1 -not -path '.' -not -path "./.config" -exec ln -s $HOME/env/root/{} $HOME/{} \;
+
 # Symlink all entries in .config
 cd $HOME/env/root/.config
 find . -maxdepth 1 -not -path '.' -exec ln -s $HOME/env/root/.config/{} $HOME/.config/{} \;
+
+# Symlink all entries in .local/bin
+cd $HOME/env/root/.local/bin
+find . -maxdepth 1 -not -path '.' -exec ln -s $HOME/env/root/.local/bin/{} $HOME/.local/bin/{} \;
 
 echo '-> Done.'

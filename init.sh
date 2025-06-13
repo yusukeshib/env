49ddc20ab12bbfe7e751f4d980b68d03cbe18462
Yusuke Shibata <yusukeshib@gmail.com>
2025-06-13 13:21:20 -0400

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-echo '-> Initializing...'
-
 # Clone the repo
 if [ ! -d "$HOME/env" ]; then
   echo '-> Cloning the repo...'
@@ -16,14 +14,25 @@ fi
 
 # Symlink all entries in the root
 cd $HOME/env/root
-find . -maxdepth 1 -not -path '.' -not -path "./.config" -exec ln -s $HOME/env/root/{} $HOME/{} \;
+for item in *; do
+  if [[ "$item" != ".config" && ! -e "$HOME/$item" ]]; then
+    ln -s "$HOME/env/root/$item" "$HOME/$item" && echo "Linked: $HOME/$item"
+  fi
+done
 
 # Symlink all entries in .config
 cd $HOME/env/root/.config
-find . -maxdepth 1 -not -path '.' -exec ln -s $HOME/env/root/.config/{} $HOME/.config/{} \;
+for item in *; do
+  if [[ ! -e "$HOME/.config/$item" ]]; then
+    ln -s "$HOME/env/root/.config/$item" "$HOME/.config/$item" && echo "Linked: $HOME/.config/$item"
+  fi
+done
 
 # Symlink all entries in .local/bin
 cd $HOME/env/root/.local/bin
-find . -maxdepth 1 -not -path '.' -exec ln -s $HOME/env/root/.local/bin/{} $HOME/.local/bin/{} \;
+for item in *; do
+  if [[ ! -e "$HOME/.local/bin/$item" ]]; then
+    ln -s "$HOME/env/root/.local/bin/$item" "$HOME/.local/bin/$item" && echo "Linked: $HOME/.local/bin/$item"
+  fi
+done
 
-echo '-> Done.'

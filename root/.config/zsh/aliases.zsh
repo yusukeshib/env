fed35a1722453292f9684ad46517cc1584867609
Yusuke Shibata <yusukeshib@gmail.com>
2025-06-17 10:07:50 -0400

    kube aliases

--- root/.config/zsh/aliases.zsh
+++ root/.config/zsh/aliases.zsh
@@ -26,22 +26,17 @@ if type "rg" > /dev/null; then
   alias rg="rg --hidden -g '!.git/'"
 fi
 
-# yay!
-alias kdv='kubectl --context=dev-view'
-alias kde='kubectl --context=dev-edit'
-alias kda='kubectl --context=dev-admin'
-alias kmv='kubectl --context=mgmt-view'
-alias kme='kubectl --context=mgmt-edit'
-alias kma='kubectl --context=mgmt-admin'
-alias kpv='kubectl --context=prod-view'
-alias kpe='kubectl --context=prod-edit'
-alias kpa='kubectl --context=prod-admin'
+if type "kubectl" > /dev/null; then
+  alias k="kubectl"
+fi
 
-alias tf='terragrunt'
-alias tfa='terragrunt apply'
-alias tfi='terragrunt init'
-alias tfiu='terragrunt init -upgrade'
-alias tfp='terragrunt plan'
+if type "kubectx" > /dev/null; then
+  alias kx="kubectx"
+fi
+
+if type "kubens" > /dev/null; then
+  alias kn="kubens"
+fi
 
 # Use gh command
 if alias gh; then

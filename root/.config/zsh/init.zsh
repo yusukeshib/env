596bf45438a897b9f5c68ccb815c618c31e4db1c
Yusuke Shibata <yusukeshib@gmail.com>
2025-08-12 08:17:04 -0400

    Clean up zshrc

--- root/.config/zsh/init.zsh
+++ root/.config/zsh/init.zsh
@@ -4,33 +4,68 @@ unsetopt BEEP
 export LANG=en_US.UTF-8
 export EDITOR="nvim"
 
+#
 # brew
+#
+
 [ -d /opt/homebrew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
 [ -d /home/linuxbrew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 
+#
 # fzf
+#
+
 export FZF_DEFAULT_COMMAND='fd --type f -i'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 source <(fzf --zsh)
 
-source "$HOME/.config/zsh/zim.zsh"
+#
+# zim
+#
 
+ZIM_HOME=${ZDOTDIR:-${HOME}}/.zim
+if [[ ! -e ${ZIM_HOME}/zimfw.zsh ]]; then
+  # Download zimfw script if missing.
+  if (( ${+commands[curl]} )); then
+    curl -fsSL --create-dirs -o ${ZIM_HOME}/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  else
+    mkdir -p ${ZIM_HOME} && wget -nv -O ${ZIM_HOME}/zimfw.zsh https://github.com/zimfw/zimfw/releases/latest/download/zimfw.zsh
+  fi
+fi
+if [[ ! ${ZIM_HOME}/init.zsh -nt ${ZDOTDIR:-${HOME}}/.zimrc ]]; then
+  # Install missing modules, and update ${ZIM_HOME}/init.zsh if missing or outdated.
+  source ${ZIM_HOME}/zimfw.zsh init -q
+fi
+source ${ZIM_HOME}/init.zsh
+
+#
 # cd replacement
+#
+
 if type "zoxide" > /dev/null; then
   eval "$(zoxide init zsh)"
 fi
 
+#
 # asdf
+#
+
 export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
+#
 # direnv
 # ensure compatibility tmux <-> direnv
+#
+
 eval "$(direnv hook bash)"
 if [ -n "$TMUX" ] && [ -n "$DIRENV_DIR" ]; then
   direnv reload
 fi
 
+#
 # kube
+#
+
 PROMPT='$(kube_ps1)'$PROMPT
 
 export PATH="$HOME/.local/bin:$PATH"

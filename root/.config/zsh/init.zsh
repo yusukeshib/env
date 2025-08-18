7fc01b357bc2ef048dd99e5f828580a28b87e530
Yusuke Shibata <yusukeshib@gmail.com>
2025-08-18 12:30:35 -0400

    gcauto

--- root/.config/zsh/init.zsh
+++ root/.config/zsh/init.zsh
@@ -87,7 +87,7 @@ function rp () {
   rg -l "$1" | xargs perl -i -pe "s|$1|$2|g"
 }
 
-function Gcauto() {
+function gcauto() {
   git commit -m "$(claude -p "Look at the staged git changes and create a summarizing git commit title. Only respond with the title and no affirmation.")"
 }
 

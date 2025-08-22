40d3ec9154382def4dabd397b0bd208f83cbde5e
Yusuke Shibata <yusukeshib@gmail.com>
2025-08-21 20:12:42 -0400

    Switch to zplug(again)

--- root/.config/zsh/init.zsh
+++ root/.config/zsh/init.zsh
@@ -3,27 +3,10 @@ unsetopt BEEP
 
 export LANG=en_US.UTF-8
 export EDITOR="nvim"
-
-#
-# brew
-#
-
-[ -d /opt/homebrew ] && eval "$(/opt/homebrew/bin/brew shellenv)"
-[ -d /home/linuxbrew ] && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-#
-# prompt
-#
-
-eval "$(starship init zsh)"
-
-#
-# fzf
-#
-
 export FZF_DEFAULT_COMMAND='fd --type f -i'
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-source <(fzf --zsh)
+
+# This is required to pass all zsh plugins
+export PATH="/opt/homebrew/bin:$PATH"
 
 #
 # zplug
@@ -34,14 +17,23 @@ export ZPLUG_HOME=/opt/homebrew/opt/zplug
 if [ -d "$ZPLUG_HOME" ]; then
   source $ZPLUG_HOME/init.zsh
     
+  zplug "plugins/asdf", from:oh-my-zsh
+  zplug "plugins/brew", from:oh-my-zsh
+  zplug "plugins/common-aliases", from:oh-my-zsh
+  zplug "plugins/direnv", from:oh-my-zsh
+  zplug "plugins/eza", from:oh-my-zsh
+  zplug "plugins/fzf", from:oh-my-zsh
+  zplug "plugins/git", from:oh-my-zsh
+  zplug "plugins/kubectl", from:oh-my-zsh
+  zplug "plugins/starship", from:oh-my-zsh
+  zplug "plugins/tmux", from:oh-my-zsh
+  zplug "plugins/zoxide", from:oh-my-zsh
+
   zplug "zsh-users/zsh-autosuggestions"
   zplug "zsh-users/zsh-history-substring-search"
   zplug "zsh-users/zsh-syntax-highlighting"
   zplug "zsh-users/zsh-completions"
   zplug "Aloxaf/fzf-tab", use:"fzf-tab.plugin.zsh"
-  zplug "plugins/git", from:oh-my-zsh
-  zplug "plugins/common-aliases", from:oh-my-zsh
-  zplug "plugins/kubectl", from:oh-my-zsh
   
   if ! zplug check --verbose; then
       printf "Install? [y/N]: "
@@ -52,30 +44,6 @@ if [ -d "$ZPLUG_HOME" ]; then
   zplug load
 fi
 
-
-#
-# cd replacement
-#
-
-if type "zoxide" > /dev/null; then
-  eval "$(zoxide init zsh)"
-fi
-
-#
-# asdf
-#
-
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
-
-#
-# direnv
-# ensure compatibility tmux <-> direnv
-#
-
-eval "$(direnv hook zsh)"
-if [ -n "$TMUX" ] && [ -n "$DIRENV_DIR" ]; then
-  direnv reload
-fi
 
 export PATH="$HOME/.local/bin:$PATH"
 
@@ -95,19 +63,6 @@ function gcauto() {
 # Aliases
 #
 
-if type "tmux" > /dev/null; then
-  alias a="tmux attach -d -t"
-  alias new="tmux new -s"
-fi
-
-if type "zoxide" > /dev/null; then
-  alias cd="z"
-fi
-
-if type "eza" > /dev/null; then
-  alias ls="eza"
-fi
-
 if type "nvim" > /dev/null; then
   alias vi="nvim"
   alias vim="nvim"
@@ -122,4 +77,3 @@ fi
 if type "rg" > /dev/null; then
   alias rg="rg --hidden -g '!.git/'"
 fi
-

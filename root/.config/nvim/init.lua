24b3b942d8e6af306747c6a2b7c95f13fde40680
Yusuke Shibata <yusukeshib@gmail.com>
2025-11-05 16:35:00 -0500

    colorscheme

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -68,40 +68,40 @@ vim.opt.completeopt = { "menuone", "noselect", "popup" }
 
 vim.pack.add({
   -- Editor behavior
-  { src = "https://github.com/tpope/vim-sleuth" },        -- Auto-detect indentation (tabs/spaces)
-  { src = "https://github.com/tpope/vim-abolish" },       -- Advanced search/replace with case variants
-  { src = "https://github.com/othree/eregex.vim" },       -- POSIX-style regular expressions
+  { src = "https://github.com/tpope/vim-sleuth" }, -- Auto-detect indentation (tabs/spaces)
+  { src = "https://github.com/tpope/vim-abolish" }, -- Advanced search/replace with case variants
+  { src = "https://github.com/othree/eregex.vim" }, -- POSIX-style regular expressions
   { src = "https://github.com/notjedi/nvim-rooter.lua" }, -- Auto-change working directory to project root
 
   -- Theme and UI
-  { src = "https://github.com/EdenEast/nightfox.nvim" },    -- Color scheme
-  { src = "https://github.com/akinsho/bufferline.nvim" },   -- Tab/buffer line at the top
+  { src = "https://github.com/scottmckendry/cyberdream.nvim" }, -- GitHub-themed color schemes
+  { src = "https://github.com/akinsho/bufferline.nvim" }, -- Tab/buffer line at the top
   { src = "https://github.com/nvim-lualine/lualine.nvim" }, -- Status line at the bottom
-  { src = "https://github.com/j-hui/fidget.nvim" },         -- LSP progress notifications
-  { src = "https://github.com/folke/which-key.nvim" },      -- Show available key bindings in popup
+  { src = "https://github.com/j-hui/fidget.nvim" }, -- LSP progress notifications
+  { src = "https://github.com/folke/which-key.nvim" }, -- Show available key bindings in popup
 
   -- Syntax and language support
   { src = "https://github.com/nvim-treesitter/nvim-treesitter" }, -- Modern syntax highlighting
 
   -- Git integration
-  { src = "https://github.com/tpope/vim-fugitive" },      -- Git commands within vim
-  { src = "https://github.com/sindrets/diffview.nvim" },  -- Enhanced diff viewer
+  { src = "https://github.com/tpope/vim-fugitive" }, -- Git commands within vim
+  { src = "https://github.com/sindrets/diffview.nvim" }, -- Enhanced diff viewer
   { src = "https://github.com/f-person/git-blame.nvim" }, -- Show git blame as virtual text
   { src = "https://github.com/lewis6991/gitsigns.nvim" }, -- Git diff signs in gutter
 
   -- File navigation and search
-  { src = "https://github.com/nvim-lua/plenary.nvim" },         -- Lua utility library (dependency)
+  { src = "https://github.com/nvim-lua/plenary.nvim" }, -- Lua utility library (dependency)
   { src = "https://github.com/nvim-telescope/telescope.nvim" }, -- Fuzzy finder (files, text, buffers)
-  { src = "https://github.com/nvim-tree/nvim-tree.lua" },       -- File explorer tree view
+  { src = "https://github.com/nvim-tree/nvim-tree.lua" }, -- File explorer tree view
 
   -- LSP (Language Server Protocol)
-  { src = "https://github.com/neovim/nvim-lspconfig" },                  -- LSP configuration presets
-  { src = "https://github.com/mason-org/mason.nvim" },                   -- Language server installer
-  { src = "https://github.com/mason-org/mason-lspconfig.nvim" },         -- Bridge mason & lspconfig
+  { src = "https://github.com/neovim/nvim-lspconfig" }, -- LSP configuration presets
+  { src = "https://github.com/mason-org/mason.nvim" }, -- Language server installer
+  { src = "https://github.com/mason-org/mason-lspconfig.nvim" }, -- Bridge mason & lspconfig
   { src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" }, -- Inline LSP diagnostics
 
   -- Code completion and AI
-  { src = "https://github.com/Saghen/blink.cmp" },       -- Completion engine
+  { src = "https://github.com/Saghen/blink.cmp" }, -- Completion engine
   { src = "https://github.com/zbirenbaum/copilot.lua" }, -- GitHub Copilot integration
 
   -- Code formatting
@@ -113,7 +113,7 @@ vim.pack.add({
 -- ============================================================================
 
 -- Apply the carbonfox color scheme
-vim.cmd.colorscheme("carbonfox")
+vim.cmd("colorscheme cyberdream")
 
 -- ============================================================================
 -- PLUGIN CONFIGURATION

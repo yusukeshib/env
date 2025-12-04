ad49280e8534480d16c920461af9fad2d884b363
Yusuke Shibata <yusukeshib@gmail.com>
2025-12-03 23:02:57 -0500

    flash

--- root/.config/nvim/init.lua
+++ root/.config/nvim/init.lua
@@ -82,6 +82,9 @@ vim.pack.add({
   -- File
   { src = "https://github.com/stevearc/oil.nvim" },
 
+  -- Enhanced search highlighting and navigation
+  { src = "https://github.com/folke/flash.nvim" },
+
   --
   -- Theme and UI
   --
@@ -211,6 +214,17 @@ require("nvim-tree").setup({
 
 -- NvimRooter: Auto-change working directory to project root
 require("nvim-rooter").setup({})
+
+require("flash").setup({
+  modes = {
+    search = {
+      enabled = true,
+    },
+    char = {
+      jump_labels = true,
+    },
+  }
+})
 
 -- Bufferline: Tab-like buffer list at top of window
 require("bufferline").setup({})
@@ -444,3 +458,10 @@ vim.keymap.set("n", "gr", telescope_lsp_refs, { noremap = true, silent = true, d
 
 -- sidekick
 vim.keymap.set({ "i", "n", "t", "x" }, "<C-.>", sidekick_toggle, { desc = "Toggle Sidekick" })
+
+-- flash
+vim.keymap.set({ "n", "x", "o" }, "s", function() require("flash").jump() end, { desc = "Flash" })
+vim.keymap.set({ "n", "x", "o" }, "S", function() require("flash").treesitter() end, { desc = "Flash Treesitter" })
+vim.keymap.set({ "o" }, "r", function() require("flash").remote() end, { desc = "Remote Flash" })
+vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
+vim.keymap.set({ "c" }, "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })

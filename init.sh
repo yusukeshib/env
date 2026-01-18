e87847ecd7bc924012184cf642d96f18b75d9707
Yusuke <yusuke@macbookpro.lan>
2026-01-17 20:01:04 -0500

    Improve init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Parse arguments
+FORCE=false
+if [ "$1" = "--force" ]; then
+  FORCE=true
+fi
+
 # Clone the repo
 if [ ! -d "$HOME/env" ]; then
   echo '-> Cloning the repo...'
@@ -9,7 +15,7 @@ fi
 
 # Symlink all files in the directory recursively, mkdir if necessary
 cd $HOME/env
-find . -type f -not -name "README.md" -not -name "init.sh" | while read -r file; do
+find . -type f -not -path "./.git/*" -not -name "README.md" -not -name "init.sh" | while read -r file; do
   # Remove leading ./
   rel_path="${file#./}"
   target="$HOME/$rel_path"
@@ -17,6 +23,16 @@ find . -type f -not -name "README.md" -not -name "init.sh" | while read -r file;
   # Create parent directory if it doesn't exist
   parent_dir="$(dirname "$target")"
   mkdir -p "$parent_dir"
+
+  # Remove broken symlink if it exists
+  if [ -L "$target" ] && [ ! -e "$target" ]; then
+    rm "$target" && echo "Removed broken symlink: $target"
+  fi
+
+  # If --force is specified, remove existing file/symlink
+  if [ "$FORCE" = true ] && [ -e "$target" ]; then
+    rm -f "$target" && echo "Removed existing file (--force): $target"
+  fi
 
   # Create symlink if it doesn't already exist
   if [ ! -e "$target" ]; then
